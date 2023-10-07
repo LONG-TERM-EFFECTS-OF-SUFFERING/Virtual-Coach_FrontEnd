8d97e556bcb54e764d0e01061be10e2d16820173
GetialJuan <getial.juan@correounivalle.edu.co>
2023-10-07 16:40:30 -0500

    Upgrade sideBar - now its responsive

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
 import logo from '../assets/logo.png'
 
+
 const Sidebar = () => {
+
+    const [showSideBar, setShowSideBar] = useState(false)
 
     const navLinks = [
         {
@@ -23,14 +27,29 @@ const Sidebar = () => {
 
     return (
         <>
-            <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm rounded-lg sm:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600">
+            <button
+                type="button"
+                className="inline-flex items-center p-2 mt-2 ml-3 text-sm rounded-lg sm:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
+                onClick={(e) => {
+                    e.stopPropagation();
+                    setShowSideBar(!showSideBar);
+                }}
+            >
                 <span className="sr-only">Open sidebar</span>
                 <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                 </svg>
             </button>
 
-            <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+            <aside
+                id="logo-sidebar"
+                aria-label="Sidebar"
+                className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${showSideBar ? "-translate-x-full" : ""} sm:translate-x-0`}
+                onPointerLeave={(e) => {
+                    e.stopPropagation();
+                    setShowSideBar(!showSideBar);
+                }}
+            >
                 <div className="h-full px-3 py-4 overflow-y-auto bg-gray-800">
                     <a href="https://flowbite.com/" className="flex items-center pl-2.5 mb-5">
                         <img src={logo} className="h-6 mr-3 sm:h-10" alt="Flowbite Logo" />
